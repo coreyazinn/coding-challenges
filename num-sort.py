702663b48e78484a96f8f91e7306ca6b2d3e4737
Corey Zinn <coreyazinn@yahoo.com>
2018-03-14 04:27:11 -0400

    inital code base

---
+++ num-sort.py
@@ -0,0 +1,29 @@
+### Stellar
+# Use camelCase or underscore_separated variable names
+# Accept an unlimited quantity of numbers, using Done as a sentinel for the end of the list
+# Provide feedback on the quantity of numbers entered
+# Provide appropriate feedback if the user enters something other than a number or the word Done
+
+
+nums = []
+
+get-nums() = 
+	nums = input("Give me a number: ")
+
+done() =
+	input()done #?
+
+print "Good morning. I am Atom your personal assistant. I can help you sort a list of numbers. Enter one number on each line, 'Done' when you are finished.
+
+"
+
+while True:
+	get-nums()
+	if done():
+		break
+
+print "Here are your 6 numbers sorted least to greatest: 
+
+", nums
+
+
